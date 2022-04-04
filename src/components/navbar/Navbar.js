83a20fcdc809597e54d66f0ebadaf4ddb7c15d48
John Almazan <almazankaze@live.com>
2022-04-03 23:47:19 -0500

    added mobile navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="main-nav">
+      <div className="menu-btn">
+        {showMenu ? (
+          <IconButton aria-label="close" onClick={toggleMenu}>
+            <CloseIcon sx={{ fontSize: 36, color: "white" }} />
+          </IconButton>
+        ) : (
+          <IconButton aria-label="menu" onClick={toggleMenu}>
+            <MenuIcon sx={{ fontSize: 36, color: "white" }} />
+          </IconButton>
+        )}
+      </div>
+
       <div class="nav-logo">
-        <h1>John Almazan</h1>
+        <h1>John</h1>
+        <h1 className="lastName">&nbsp;Almazan</h1>
       </div>
 
       <ul class="nav-links">
@@ -26,7 +47,7 @@ const Navbar = () => {
 
       <ul class="nav-social-icons">
         <li>
-          <IconButton aria-label="add">
+          <IconButton aria-label="linkedin">
             <LinkedInIcon
               sx={{
                 fontSize: 28,
@@ -36,7 +57,7 @@ const Navbar = () => {
           </IconButton>
         </li>
         <li>
-          <IconButton aria-label="add">
+          <IconButton aria-label="github">
             <GitHubIcon
               sx={{
                 fontSize: 28,
@@ -46,7 +67,7 @@ const Navbar = () => {
           </IconButton>
         </li>
         <li>
-          <IconButton aria-label="add">
+          <IconButton aria-label="youtube">
             <YouTubeIcon
               sx={{
                 fontSize: 28,
@@ -56,6 +77,53 @@ const Navbar = () => {
           </IconButton>
         </li>
       </ul>
+
+      <div className={showMenu ? "mobile-nav-menu show" : "mobile-nav-menu"}>
+        <ul className="mobile-nav-links">
+          <li>
+            <a href="#Projects">Projects</a>
+          </li>
+          <li>
+            <a href="#Technologies">Technologies</a>
+          </li>
+          <li>
+            <a href="#About">About</a>
+          </li>
+        </ul>
+
+        <ul className="mobile-icon-links">
+          <li>
+            <IconButton aria-label="linkedin">
+              <LinkedInIcon
+                sx={{
+                  fontSize: 28,
+                  color: "white",
+                }}
+              />
+            </IconButton>
+          </li>
+          <li>
+            <IconButton aria-label="github">
+              <GitHubIcon
+                sx={{
+                  fontSize: 28,
+                  color: "white",
+                }}
+              />
+            </IconButton>
+          </li>
+          <li>
+            <IconButton aria-label="youtube">
+              <YouTubeIcon
+                sx={{
+                  fontSize: 28,
+                  color: "white",
+                }}
+              />
+            </IconButton>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
