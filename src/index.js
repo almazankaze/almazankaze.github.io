8890764508b4f73f0d4af80c39f67597024de487
John Almazan <almazankaze@live.com>
2022-09-30 00:37:12 -0500

    updated css for header and added reset css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import "./reset.css";
 import "./index.css";
 
 const container = document.getElementById("root");
