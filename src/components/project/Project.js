eebc4e12757cb6a4376d71806bc7893f468b7a80
John Almazan <almazankaze@live.com>
2022-10-01 01:25:09 -0500

    updated look of cards

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -3,10 +3,14 @@ import React from "react";
 import "./project.css";
 
 const Project = ({ project }) => {
+  const backgroundImg = {
+    backgroundImage: `url(${project.img})`,
+  };
+
   return (
     <div className="project-card">
       <div className="card-content">
-        <img src={project.img} alt="alt" />
+        <div className="card-image" style={backgroundImg}></div>
         <h3>{project.title}</h3>
         <hr></hr>
         <p>{project.description}</p>
