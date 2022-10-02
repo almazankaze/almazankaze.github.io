044ed2953c56db1ee8934c37904afe90fc705fb3
John Almazan <almazankaze@live.com>
2022-10-01 23:44:38 -0500

    updated navbar css

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
-import IconButton from "@mui/material/IconButton";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import YouTubeIcon from "@mui/icons-material/YouTube";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import "./navbar.css";
+import SocialLinks from "./SocialLinks";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
+  const openMenu = () => {
+    setShowMenu(true);
   };
 
   const closeMenu = () => {
@@ -20,139 +17,53 @@ const Navbar = () => {
 
   return (
     <div className="main-nav">
-      <div className="menu-btn">
-        {showMenu ? (
-          <IconButton aria-label="close" onClick={toggleMenu}>
-            <CloseIcon sx={{ fontSize: 36, color: "white" }} />
-          </IconButton>
-        ) : (
-          <IconButton aria-label="menu" onClick={toggleMenu}>
-            <MenuIcon sx={{ fontSize: 36, color: "white" }} />
-          </IconButton>
-        )}
-      </div>
-
-      <div className="nav-logo">
+      <div className="left-nav">
         <h1>John</h1>
         <h1 className="lastName">&nbsp;Almazan</h1>
       </div>
 
-      <ul className="nav-links">
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#technology">Technologies</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-      </ul>
-
-      <ul className="nav-social-icons">
-        <li>
-          <IconButton
-            aria-label="linkedin"
-            href="https://linkedin.com/in/john-almazan/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <LinkedInIcon
-              sx={{
-                fontSize: 28,
-                color: "white",
-              }}
-            />
-          </IconButton>
-        </li>
-        <li>
-          <IconButton
-            aria-label="github"
-            href="https://github.com/almazankaze"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GitHubIcon
-              sx={{
-                fontSize: 28,
-                color: "white",
-              }}
-            />
-          </IconButton>
-        </li>
-        <li>
-          <IconButton aria-label="youtube">
-            <YouTubeIcon
-              sx={{
-                fontSize: 28,
-                color: "white",
-              }}
-            />
-          </IconButton>
-        </li>
-      </ul>
-
-      <div className={showMenu ? "mobile-nav-menu show" : "mobile-nav-menu"}>
-        <ul className="mobile-nav-links">
-          <li>
+      <div className={showMenu ? "middle-nav show" : "middle-nav"}>
+        <h1 className="sidebar-name">John</h1>
+        <ul className="middle-nav-menu">
+          <li className="nav-link">
             <a href="#projects" onClick={closeMenu}>
               Projects
             </a>
           </li>
-          <li>
+          <li className="nav-link">
             <a href="#technology" onClick={closeMenu}>
               Technologies
             </a>
           </li>
-          <li>
+          <li className="nav-link">
             <a href="#about" onClick={closeMenu}>
               About
             </a>
           </li>
         </ul>
+        <div className="mobile-social-links">
+          <SocialLinks />
+        </div>
+      </div>
 
-        <ul className="mobile-icon-links">
-          <li>
-            <IconButton
-              aria-label="linkedin"
-              href="https://linkedin.com/in/john-almazan/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedInIcon
-                sx={{
-                  fontSize: 28,
-                  color: "white",
-                }}
-              />
-            </IconButton>
-          </li>
-          <li>
-            <IconButton
-              aria-label="github"
-              href="https://github.com/almazankaze"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GitHubIcon
-                sx={{
-                  fontSize: 28,
-                  color: "white",
-                }}
-              />
-            </IconButton>
-          </li>
-          <li>
-            <IconButton aria-label="youtube">
-              <YouTubeIcon
-                sx={{
-                  fontSize: 28,
-                  color: "white",
-                }}
-              />
-            </IconButton>
-          </li>
-        </ul>
+      <div className="right-nav">
+        <SocialLinks />
+      </div>
+
+      <div className="menu-btn">
+        {showMenu ? (
+          <CloseIcon
+            className="menu-icon"
+            onClick={closeMenu}
+            titleAccess="close"
+          />
+        ) : (
+          <MenuIcon
+            className="menu-icon"
+            onClick={openMenu}
+            titleAccess="menu"
+          />
+        )}
       </div>
     </div>
   );
